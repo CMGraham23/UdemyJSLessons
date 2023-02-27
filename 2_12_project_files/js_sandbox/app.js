02c381e1fea7146f8efa55dca1f7f0ab5a0a2268
Christine Murray Graham <cmurrayjm@gmail.com>
2023-02-27 04:58:10 -0800

    my javascript lesson

---
+++ 2_12_project_files/js_sandbox/app.js
@@ -0,0 +1,138 @@
+// if(something){
+//   do something
+// } else {
+//   do something else
+// }
+// single equal sign is an assignment
+// === is a comparison
+
+
+const id = 100;
+
+
+
+
+
+// // EQUAL TO
+// if(id == 100){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // NOT EQUAL TO
+// if(id != 101){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // EQUAL TO VALUE & TYPE
+// if(id === 100){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+
+// // EQUAL TO VALUE & TYPE
+// if(id !== 100){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// Test if undefined
+// if(typeof id !== 'undefined'){
+//   console.log(`The ID is ${id}`);
+// } 
+// else {
+//   console.log('NO ID');
+// }
+
+// GREATER OR LESS THAN
+// if(id <= 100){
+//   console.log('CORRECT');
+// } 
+// else {
+//   console.log('INCORRECT');
+// }
+
+
+
+
+// IF ELSE
+
+// const color = 'yellow';
+
+//  if(color === 'red'){
+//  console.log('Color is red');
+//  }
+//  else if(color === 'blue'){
+//  console.log('Color is blue');
+//  }
+//  else {
+//   console.log('Color is not red or blue');
+//  } 
+ 
+
+
+// const Name = 'Steve';
+// const age = 40;
+// if(age > 0 && age < 12){
+//   console.log(`${Name} is a child`);
+// }
+// else if (age >= 13 && age <= 19){
+//   console.log(`${Name} is a teenager`);
+// }
+// else {
+//   console.log(`${Name} is an  adult`);
+// }
+
+
+
+
+
+
+
+
+
+
+// LOGICAL OPERATORS
+
+// const name = 'Steve';
+// const age = 70;
+
+// AND && - ensures both conditions are true
+// if(age > 0 && age < 12){
+//   console.log(`${name} is a child`);
+// } else if(age >= 13 && age <= 19){
+//   console.log(`${name} is a teenager`);
+// } else {
+//   console.log(`${name} is an adult`);
+// }
+
+// // OR || - only one condition needs to be true
+// if(age < 16 || age > 65){
+//   console.log(`${name} can not run in race`);
+// } else {
+//   console.log(`${name} is registered for the race`);
+// }
+
+
+
+
+// // TERNARY OPERATOR represented with (?), ELSE OPERATOR represented with (:) - writing shorthand statement
+
+// console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
+
+// // WITHOUT BRACES
+// if(id === 100)
+//   console.log('CORRECT');
+// else
+//   console.log('INCORRECT');
+
+// if(id === 100)
+//   console.log('CORRECT');
+// else
+//   console.log('INCORRECT');
